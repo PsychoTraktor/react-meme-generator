7de0098d2ca876b68f542eef049888ede9fbb9bc
Roland Banfi <rollike2@gmail.com>
2020-01-18 22:39:55 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function () {
+    return (
+        <header>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"></img>
+            <p>Meme Generator</p>
+        </header>
+    )
+}
